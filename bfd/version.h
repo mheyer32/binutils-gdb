953071f0c391484359635858651ac52bfd890b66
fautomat <automat@franke.ms>
2019-03-18 16:56:24 +0100

    bump version date

--- bfd/version.h
+++ bfd/version.h
@@ -1,4 +1,4 @@
-#define BFD_VERSION_DATE 190317-223657
+#define BFD_VERSION_DATE 190318-165624
 #define BFD_VERSION @bfd_version@
 #define BFD_VERSION_STRING @bfd_version_package@ @bfd_version_string@
 #define REPORT_BUGS_TO @report_bugs_to@
