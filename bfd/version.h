61f63cd0a3b967b13a4f6d773524e60edc1fc7d9
fautomat <automat@franke.ms>
2020-07-04 15:19:21 +0000

    bump version date

--- bfd/version.h
+++ bfd/version.h
@@ -1,4 +1,4 @@
-#define BFD_VERSION_DATE 200419-165903
+#define BFD_VERSION_DATE 200704-151921
 #define BFD_VERSION @bfd_version@
 #define BFD_VERSION_STRING @bfd_version_package@ @bfd_version_string@
 #define REPORT_BUGS_TO @report_bugs_to@
