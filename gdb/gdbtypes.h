a18409605ab98fead9caca1e915b237e46e28bb8
bebbo <bebbo@bejy.net>
2020-12-29 22:53:10 +0100

    fix NPE

--- gdb/gdbtypes.h
+++ gdb/gdbtypes.h
@@ -904,7 +904,7 @@ struct type
   /* Get the name of this type.  */
   const char *name () const
   {
-    return this->main_type->name;
+    return this->main_type->name ? this->main_type->name : "__??";
   }
 
   /* Set the name of this type.  */
