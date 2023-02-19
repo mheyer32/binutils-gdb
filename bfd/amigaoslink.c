456c57ff673ea97eb118a79fedf93ec42211fa63
bebbo <bebbo@bejy.net>
2023-02-19 19:35:39 +0100

    check only .text section to enable hard relocation

--- bfd/amigaoslink.c
+++ bfd/amigaoslink.c
@@ -1280,7 +1280,7 @@ amiga_final_link (
 
   /* SBF: check if some offset is given, then all relocs are resolved using output_section->vma. */
   for (o = abfd->sections->output_section; o != NULL; o = o->next)
-    if (o->vma != 0 && (o->prev == NULL || o->prev->size + o->prev->vma != o->vma))
+    if (o->vma != 0 && (o->prev == NULL || o->prev->size + o->prev->vma != o->vma) && 0 == strncmp(".text", o->name, 5))
       {
 	AMIGA_DATA(abfd)->vma_reloc = true;
 	break;
