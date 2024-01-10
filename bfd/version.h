9fccb70d1352347bd340fd77ab0e9b8315147293
fautomat <automat@franke.ms>
2024-01-10 18:00:44 +0100

    bump version date

--- bfd/version.h
+++ bfd/version.h
@@ -1,4 +1,4 @@
-#define BFD_VERSION_DATE 240109-124603
+#define BFD_VERSION_DATE 240110-180043
 #define BFD_VERSION @bfd_version@
 #define BFD_VERSION_STRING @bfd_version_package@ @bfd_version_string@
 #define REPORT_BUGS_TO @report_bugs_to@
